39b458dcc8094a4b67d43e5592345c144d0576cf
manal <manalmadaniy@gmail.com>
2020-11-11 22:31:34 +0100

    Updates

---
+++ precache-manifest.7e5a011c9a5ccacd3c50c7edb88c9ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cde81880984b57e1dc33c9605faa43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd354eb8c359002cba81",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36dc4c21ac5707915aff",
+    "url": "/static/js/2.1288cb2e.chunk.js"
+  },
+  {
+    "revision": "fd354eb8c359002cba81",
+    "url": "/static/js/main.4bb31a21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a2471d78460501bd03b1c76e150e8395",
+    "url": "/static/media/profile.a2471d78.jpg"
+  }
+]);
